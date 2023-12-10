ef94d6668de21c2c1ede0eb009eb2cd1c4625656
Erisha Roach <forever.erisha@gmail.com>
2023-12-10 14:47:38 -0500

    part 2 done. started part 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Part 1: Humble Beginnings
+console.log('==== Part 1: Humble Beginnings ====');
 
 const adventurer = {
     name: 'Robin',
@@ -38,3 +38,41 @@ console.log(adventurer);
 adventurer.roll();
 adventurer.roll();
 adventurer.roll();
+
+console.log(' ====Part 2: Class Fantasy ====');
+
+class Character {
+    constructor (name){
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+
+    }
+    roll(mod = 0){
+        const result = Math.floor(Math.random() * 20) + 1
+        console.log(`${this.name} rolled a ${result}`);
+    } 
+}
+const robin = new Character ('Robin');
+robin.inventory = ['sword', 'potion', 'artifact'];
+robin.companion = new Character ('Leo');
+robin.companion.type = 'Cat';
+robin.companion.companion = new Character ('Frank');
+robin.companion.companion.type = 'Flea';
+robin.companion.companion.inventory = ['small hat', 'sunglasses']
+
+console.log(robin); 
+console.log(robin.roll()); 
+
+console.log('==== Part 3: Class Features ====');
+
+class Adventurer extends Character {
+    constructor (name, role) {
+        super (name); 
+
+        // adventurer specialized role
+        this.role = role;
+
+        this.inventory.push('snacks', '50 gold coins')
+    }
+}
